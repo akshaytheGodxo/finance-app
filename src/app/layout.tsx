7b4936d9c59073600f6abfbb8b7f7d245613f125
akshaytheGodxo <akshaysinghc230@gmail.com>
2025-02-01 19:54:08 +0530

    bas navbar banaya hai

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import "~/styles/globals.css";
-
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
-
+import { ThemeProvider } from "next-themes";
 import { TRPCReactProvider } from "~/trpc/react";
 
 export const metadata: Metadata = {
@@ -16,8 +15,12 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+      <body data-theme="light">
+          <ThemeProvider attribute="class">  
+        <TRPCReactProvider>
+            {children}
+        </TRPCReactProvider>
+          </ThemeProvider>
       </body>
     </html>
   );
